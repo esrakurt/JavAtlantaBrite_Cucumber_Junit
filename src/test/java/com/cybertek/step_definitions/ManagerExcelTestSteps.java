a916c5abc5e48e4024fea7b4cd704d8e7b176e93
Levent Ozturk <lozturkk987@gmail.com>
2019-03-07 12:37:06 -0500

    third excel test

--- src/test/java/com/cybertek/step_definitions/ManagerExcelTestSteps.java
+++ src/test/java/com/cybertek/step_definitions/ManagerExcelTestSteps.java
@@ -3,6 +3,10 @@ package com.cybertek.step_definitions;
 import com.cybertek.utilities.ExcelUtil;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
 
 public class ManagerExcelTestSteps {
 
@@ -26,8 +30,12 @@ public class ManagerExcelTestSteps {
 
     @Then("verify that spreadsheet contains {string}")
     public void verify_that_spreadsheet_contains(String string) {
-
-
+        String file = "./src/test/resources/testData/ManagerExcel.xls";
+        String sheet = "quotSpecs";
+        ExcelUtil quotationData = new ExcelUtil(file, sheet);
+        //List<Map<String, String>> allDataInExcel = quotationData.getDataList();
+        List<String> columnData = quotationData.getColumnData(0);
+        Assert.assertTrue(columnData.contains("Customer"));
     }
 
 
